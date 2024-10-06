25b11f5221ab8af5b32f60a7ba26f62e71f189e5
Giorgi Beridze <98933921+beridzeg45@users.noreply.github.com>
2024-10-06 20:15:30 +0400

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def get_world_graph(city_list):
     ax.set_facecolor(plot_color)
     fig.patch.set_facecolor(plot_color)
     world.plot(ax=ax, color=plot_color, ec='white', lw=0.1, alpha=1.0)
-    ax.scatter(x=[coord['Lon'] for city, coord in city_coordinates.items()],y=[coord['Lat'] for city, coord in city_coordinates.items()],s=0.1,c=[coord['Lat'] if coord['Lat'] is not None and coord['Lat'] >= 0 else -1 * coord['Lat'] if coord['Lat'] is not None else 0 for city, coord in city_coordinates.items()],alpha=1.0)
+    ax.scatter(x=[coord['Lon'] for city, coord in city_coordinates.items()],y=[coord['Lat'] for city, coord in city_coordinates.items()],s=0.1,c=[coord['Lat'] if coord['Lat'] is not None and coord['Lat'] >= 0 else -1 * coord['Lat'] if coord['Lat'] is not None else 0 for city, coord in city_coordinates.items()],alpha=1.0,cmap='hot_r')
     ax.scatter(x=lons,y=lats,s=30,c=cmap[:len(city_list)])
     #for i in range(len(cities)):
     #    ax.text(lons[i],lats[i],cities[i],fontweight='bold',color='white',s=6)

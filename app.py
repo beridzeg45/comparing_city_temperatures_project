c691026c0427502236d5e33fe9272875bc8305a6
Giorgi Beridze <98933921+beridzeg45@users.noreply.github.com>
2024-10-06 19:45:42 +0400

    Update app.py

--- app.py
+++ app.py
@@ -92,12 +92,16 @@ def get_world_graph(city_list):
     plot_color = (14/255, 17/255, 23/255)
     ax.set_facecolor(plot_color)
     fig.patch.set_facecolor(plot_color)
-
     world.plot(ax=ax, color=plot_color, ec='white', lw=0.1, alpha=1.0)
-
-  
-    
+    ax.scatter(x=[coord['Lon'] for city,coord in city_coordinates.items()],
+        y=[coord['Lat'] for city,coord in city_coordinates.items()],
+        s=0.2,
+        c=[coord['Lat'] if coord['Lat'] is not None and coord['Lat']>=0 else -1*coord['Lat']  if coord['Lat'] is not None else 0 for city,coord in city_coordinates.items()],cmap='hot_r')
+    plot_color=(14/255, 17/255, 23/255)
+    ax.set_facecolor(plot_color)
+    fig.patch.set_facecolor(plot_color) 
     ax.axis('off')
+      
 
 
 

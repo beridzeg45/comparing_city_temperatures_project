15a88fd057cbcfe2bb7868e4f5f26f7eb19bc300
Giorgi Beridze <98933921+beridzeg45@users.noreply.github.com>
2024-10-06 20:04:11 +0400

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def get_world_graph(city_list):
     world.plot(ax=ax, color=plot_color, ec='white', lw=0.1, alpha=1.0)
     ax.scatter(x=lons,y=lats,s=10,c=cmap[:len(city_list)])
     for i in range(len(cities)):
-        ax.text(lons[i],lats[i],cities[i].split(' (')[0],fontweight='bold',color='white',s=6)
+        ax.text(lons[i],lats[i],cities[i],fontweight='bold',color='white',s=6)
     plot_color=(14/255, 17/255, 23/255)
     ax.set_facecolor(plot_color)
     fig.patch.set_facecolor(plot_color) 

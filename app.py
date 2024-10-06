3e5a6d9ca8c4c724ceca0d610332e8eaf3520159
Giorgi Beridze <98933921+beridzeg45@users.noreply.github.com>
2024-10-06 19:24:58 +0400

    Update app.py

--- app.py
+++ app.py
@@ -68,14 +68,14 @@ def get_temp_graph(city_list):
 
 def default_world_map():
     fig,ax=plt.subplots(figsize=(7,7))
-    plot_color=(14/255, 17/255, 23/255)
-    ax.set_facecolor(plot_color)
-    fig.patch.set_facecolor(plot_color) 
     world.plot(ax=ax, color='black', ec='black', lw=0.1, alpha=1.0)
     ax.scatter(x=[coord['Lon'] for city,coord in city_coordinates.items()],
             y=[coord['Lat'] for city,coord in city_coordinates.items()],
             s=0.2,
             c=[coord['Lat'] if coord['Lat'] is not None and coord['Lat']>=0 else -1*coord['Lat']  if coord['Lat'] is not None else 0 for city,coord in city_coordinates.items()],cmap='hot_r')
+    plot_color=(14/255, 17/255, 23/255)
+    ax.set_facecolor(plot_color)
+    fig.patch.set_facecolor(plot_color) 
     ax.axis('off')
 
 
